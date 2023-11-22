2cf7b95b5cdded9d8f98ca1541e3327ac6026e95
Lucas Huerta <huertalucas13@gmail.com>
2023-11-22 15:55:52 +0100

    [ADD] First page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,95 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main-container">
+    <div class="container-text">
+      <h1>
+        Transform your Sounds into Canvases of Visual Emotions
+      </h1>
+      <div class="container-input">
+        <p>
+          Drop your file here
+        </p>
+        <div class="container-component-input">
+          <input type="file" placeholder="Select your file" />
+          <button type="button">
+            Upload
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="top-blur-circle">
+    </div>
+    <div class="bottom-blur-circle">
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.container-text {
+  width: 70%;
+  height: 50%;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.container-text h1 {
+  text-align: center;
+}
+
+.container-input {
+  height: 70%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+}
+
+.container-component-input {
+  width: 100%;
+  padding: 8px 24px;
+  background-color: white;
+  border: 1px solid white;
+  border-radius: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.container-component-input button {
+  background: rgba(42, 56, 182, 0.87);
+  color: white;
+}
+
+.top-blur-circle,
+.bottom-blur-circle {
+  position: absolute;
+  width: 567px;
+  height: 567px;
+  flex-shrink: 0;
+  border-radius: 567px;
+  background: linear-gradient(180deg, #242C77 0%, rgba(222, 222, 228, 0.59) 100%);
+  filter: blur(20px);
+}
+
+.top-blur-circle {
+  bottom: 50%;
+  right: 85%;
+}
+
+.bottom-blur-circle {
+  top: 50%;
+  left: 85%;
 }
 </style>
